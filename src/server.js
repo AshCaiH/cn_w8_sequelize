01c342b73cdffad096194182ec50f3ea76497c9b
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-02-13 12:29:09 +0000

    chore: add syncTables to server.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,26 @@
 require("dotenv").config();
 const express = require("express");
 
+const Book = require("./books/model");
+
+
 const port = process.env.PORT || 5001;
 
 const app = express();
 
 app.use(express.json());
 
+const syncTables = async () => {
+    await Book.sync();
+    console.log("Tables synced");
+}
+
 app.get("/health", (req, res) => {
     res.status(200).json({message:"API is healthy"});
 })
 
 app.listen(port, () => {
+    syncTables();
     console.log(`Server is listening on port ${port}`)
     console.log(`Page accessible at http://localhost/${port}`)
 });
