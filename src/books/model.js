79dcbf5141dbcb83375f39b71f433985f646abcf
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-02-13 14:00:23 +0000

    chore: disable timestamps in database

--- src/books/model.js
+++ src/books/model.js
@@ -1,20 +1,23 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../db/connection");
 
-const Book = sequelize.define("Book", {
-    title: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false
+const Book = sequelize.define(
+    "Book", {
+        title: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false
+        },
+        author: {
+            type: DataTypes.STRING,
+            defaultValue: "Author unspecified"
+        },
+        genre: {
+            type: DataTypes.STRING,
+            defaultValue: "Genre unspecified"
+        },
     },
-    author: {
-        type: DataTypes.STRING,
-        defaultValue: "Author unspecified"
-    },
-    genre: {
-        type: DataTypes.STRING,
-        defaultValue: "Genre unspecified"
-    }
-});
+    { timestamps: false },
+);
 
 module.exports = Book;
