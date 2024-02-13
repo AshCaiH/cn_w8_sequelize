71647e1eb0725278e54a73f97ed9b4f3f96598ea
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-02-13 15:15:30 +0000

    refactor: create sendError function to reduce repetition

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -1,7 +1,14 @@
 const Book = require("./model")
 
+const sendError = (res, error) => {
+    res.status(500).json({
+        message: error.message,
+        error: error,
+    });
+}
 
 module.exports = {
+    // Create
     addBook: async (req, res) => {
         try {
             const book = await Book.create({
@@ -10,17 +17,28 @@ module.exports = {
                 genre:  req.body.genre,
             });
 
+            res.status(200).json({
+                message: 'Successfully added book to database.', 
+                book:book 
+            });
+        } catch (error) {sendError(res, error)};
+    },
+
+    // Read
+    readBooks: async (req, res) => {
+        try {            
+            const books = await Book.findAll();
+
             res.status(200).json(
                 {
-                    message: 'Successfully added book to database.', 
-                    book:book 
+                    message: 'List of books successfully retreived.', 
+                    books: books 
                 }
             );
-        } catch (error) {
-            res.status(500).json({
-                message: error.message,
-                error: error,
-            });
-        };
-    }
+        } catch (error) {sendError(res, error)};
+    },
+
+    // Update
+
+    // Delete
 }
